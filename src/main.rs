a8359a31bed84049a1704b9d0a2a66a1c3165bfc
Jessy DROUIN <jessy.drouin@equasens.com>
2024-12-17 15:30:11 +0100

    add function + tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+
 fn main() {
-    println!("Hello, world!");
+    // Add
+    add(2, 2);
+
+}
+
+fn add(x:i8, y: i8) -> i8 {
+    x + y
+}
+
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        let result = add(2, 2);
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn another() {
+        panic!("FAIL BON SANG");
+    }
+    
+
+
 }
